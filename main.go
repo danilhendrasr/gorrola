9574d25070809294b91bc11bafcdd4cc6817de28
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-01-18 14:56:15 +0700

    feat: initial load balancing implementation

--- main.go
+++ main.go
@@ -6,12 +6,34 @@ import (
 	"net/url"
 )
 
+var serverUrls = []string{"http://localhost:8080", "http://localhost:8081", "http://localhost:8082"}
+
+type BackendPool struct {
+	backends []*httputil.ReverseProxy
+	current  uint64
+}
+
+var pp = BackendPool{
+	[]*httputil.ReverseProxy{},
+	0,
+}
+
+func balanceLoad(w http.ResponseWriter, r *http.Request) {
+	pp.backends[pp.current].ServeHTTP(w, r)
+	pp.current = (pp.current + 1) % uint64(len(pp.backends))
+}
+
 func main() {
-	u, _ := url.Parse("http://localhost:8080")
+	for _, serverUrl := range serverUrls {
+		u, _ := url.Parse(string(serverUrl))
+		rp := httputil.NewSingleHostReverseProxy(u)
+		pp.backends = append(pp.backends, rp)
+	}
 
-	rp := httputil.NewSingleHostReverseProxy(u)
-	handler := http.HandlerFunc(rp.ServeHTTP)
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(balanceLoad),
+	}
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	server.ListenAndServe()
 }
